db8a50d63b32441811fddc7e438ccef27d7a8dce
커피볶는정콩 <61773021+DevelopJKong@users.noreply.github.com>
2022-07-28 09:39:50 +0900

    확인 완료 [백엔드 연결 확인 필요]

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { WebView } from "react-native-webview";
+import styled from "styled-components/native";
+
+
+const MarginBox = styled.View`
+  margin-top:150px;
+`;
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <MarginBox></MarginBox>
+            <WebView source={{ uri: "https://developjkong.github.io/exchange" }} />
+        </>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
